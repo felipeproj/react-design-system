b7689448a05b3f457efc845add225a3997c90414
Luís Felipe <luisfelipenovais98@gmail.com>
2022-10-12 10:52:20 -0300

    chore: Updating Text stories

--- src/components/text/Text.stories.tsx
+++ src/components/text/Text.stories.tsx
@@ -5,20 +5,27 @@ export default {
     title: 'Components/Text',
     component: Text,
     args: {
-        children: 'Lorem ipsum.'
+        children: 'Lorem ipsum.',
+        size: 'md'
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
     }
 } as Meta<TextProps>;
 
-export const Default: StoryObj<TextProps> = {
-    args: {
-        size: 'md'
-    }
-}
+export const Default: StoryObj<TextProps> = {}
+
 export const Small: StoryObj<TextProps> = {
     args: {
         size: 'sm'
     }
 }
+
 export const Large: StoryObj<TextProps> = {
     args: {
         size: 'lg'
@@ -29,5 +36,12 @@ export const CustomComponent: StoryObj<TextProps> = {
     args: {
         asChild: true,
         children: (<p>Teste</p>)
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true
+            }
+        }
     }
 }
