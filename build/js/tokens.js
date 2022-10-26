5248c8a6eb234c053bdb07965b66243fc94e00e1
Luís Felipe <luisfelipenovais98@gmail.com>
2022-10-26 20:01:51 -0300

    feat: Adding style-dictionary configs

---
+++ build/js/tokens.js
@@ -0,0 +1,15 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 26 Oct 2022 22:59:16 GMT
+ */
+
+module.exports = {
+  "ColorBlack": "#000000",
+  "ColorGray100": "#e1e1e6",
+  "ColorGray200": "#c4c4cc",
+  "ColorGray400": "#7c7c8a",
+  "ColorGray800": "#202024",
+  "ColorGray900": "#121214",
+  "ColorCyan300": "#9be1fb",
+  "ColorCyan500": "#81d8f7"
+};
